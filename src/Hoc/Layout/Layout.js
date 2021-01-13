f3943eb8ef85886d37a5c02b7f9de80b86b32594
kislykamil <kislykamil@gmail.com>
2021-01-13 18:38:06 +0100

    added hoc layout + installed routing

---
+++ src/Hoc/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+// import './Layout.css'
+import Aux from '../Auxiliary/Auxiliary';
+
+
+class Layout extends Component {
+
+    render() {
+
+        return (
+            <Aux>
+                {/* SOME NAV */}
+            </Aux>
+        )
+    }
+}
+
+
+export default Layout;
