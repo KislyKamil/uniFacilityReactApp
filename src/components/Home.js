93eedafd1ed635646f0a0be88595e11a07d5c6f9
kislykamil <kislykamil@gmail.com>
2021-01-20 23:23:01 +0100

    added news to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {MDBContainer} from "mdbreact";
 import Register from "./Register";
 import Departments from "./Departments";
+import HomePageNews from "./UI/HomePageNews";
 
 const Home = () => {
 
@@ -10,6 +11,11 @@ const Home = () => {
         <div>
             <Register></Register>
             <Departments></Departments>
+            <MDBContainer>
+                <HomePageNews text1={"Ostatania widomość"} text2={"Ostatania widomość2"}/>
+                <HomePageNews text1={"Ostatania widomość"} text2={"Ostatania widomość2"}/>
+            </MDBContainer>
+
         </div>
     )
 }
---
+++ src/components/UI/HomePageNews.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {MDBCard, MDBCardBody, MDBCardFooter, MDBCardText, MDBCardTitle, MDBCol, MDBRow} from "mdbreact";
+
+const HomePageNews = (props) => {
+    return (
+        <MDBRow>
+            <MDBCol>
+                <MDBCard>
+                    <MDBCardBody>
+                        <MDBCardTitle tag="h5">{props.text1}</MDBCardTitle>
+                        <MDBCardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum
+                            orci in faucibus lobortis. Sed a mollis mi, id vestibulum urna. Praesent ante enim,
+                        </MDBCardText>
+                    </MDBCardBody>
+                    <MDBCardFooter small muted>
+                        07.01.2021
+                    </MDBCardFooter>
+                </MDBCard>
+            </MDBCol>
+            <MDBCol>
+                <MDBCard>
+                    <MDBCardBody>
+                        <MDBCardTitle tag="h5">{props.text2}</MDBCardTitle>
+                        <MDBCardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum
+                            orci in faucibus lobortis. Sed a mollis mi, id vestibulum urna. Praesent ante enim,
+                        </MDBCardText>
+                    </MDBCardBody>
+                    <MDBCardFooter small muted>
+                        01.01.2021
+                    </MDBCardFooter>
+                </MDBCard>
+            </MDBCol>
+            <MDBCol>
+                <MDBCard>
+                    <MDBCardBody>
+                        <MDBCardTitle tag="h5">{props.text2}</MDBCardTitle>
+                        <MDBCardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum
+                            orci in faucibus lobortis. Sed a mollis mi, id vestibulum urna. Praesent ante enim,
+                        </MDBCardText>
+                    </MDBCardBody>
+                    <MDBCardFooter small muted>
+                        01.01.2021
+                    </MDBCardFooter>
+                </MDBCard>
+            </MDBCol>
+            <MDBCol>
+                <MDBCard>
+                    <MDBCardBody>
+                        <MDBCardTitle tag="h5">{props.text2}</MDBCardTitle>
+                        <MDBCardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum
+                            orci in faucibus lobortis. Sed a mollis mi, id vestibulum urna. Praesent ante enim,
+                        </MDBCardText>
+                    </MDBCardBody>
+                    <MDBCardFooter small muted>
+                        01.01.2021
+                    </MDBCardFooter>
+                </MDBCard>
+            </MDBCol>
+        </MDBRow>
+    )
+}
+
+export default HomePageNews
